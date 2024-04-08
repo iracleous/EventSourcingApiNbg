74cb867e58af8bc1e2adb76e614925f23c980029
dimitris <iracleous@gmail.com>
2024-04-08 11:06:30 +0300

    refactoring

--- EventSourcingApiNbg/Controllers/EventController.cs
+++ EventSourcingApiNbg/Controllers/EventController.cs
@@ -1,7 +1,4 @@
-﻿ 
-
-namespace EventSourcingApiNbg.Controllers;
-
+﻿namespace EventSourcingApiNbg.Controllers;
 
 using System;
 using System.Threading.Tasks;
@@ -10,8 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using EventSourcingApiNbg.Models;
 using System.Text.Json;
-
-using System.Collections;
 using System.Text;
 using System.Text.Json.Serialization;
 
@@ -26,34 +21,16 @@ using System.Text.Json.Serialization;
             _eventStoreClient = eventStoreClient ?? throw new ArgumentNullException(nameof(eventStoreClient));
         }
 
-
-
     [HttpGet("read/{streamName}")]
     public async Task<IActionResult> ReadEventsFromStream([FromRoute] string streamName)
     {
-        // Deserialize JSON with custom options
-        var options = new JsonSerializerOptions
-        {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            Converters = { new JsonStringEnumConverter() } 
-            // Optional: Include this line if you have enums in your object
-        };
-
         try
         {
-            var events = new List<WeatherForecast>();
+            var events = new List<WeatherForecast?>();
             // Read events from the specified stream starting from the beginning
-            await _eventStoreClient.ReadStreamAsync(
-              Direction.Forwards, streamName, StreamPosition.Start)
-                .ForEachAsync(
-                     ev=> {
-                         ////// to be correctly deserialized
-                         var byteArray = ev.Event.Data;
-                         string decodedString = Encoding.UTF8.GetString(byteArray.ToArray());
-                         WeatherForecast vv = JsonSerializer.Deserialize<WeatherForecast>(decodedString, options);
-                          events.Add(vv);
-                     });
-
+            await _eventStoreClient
+                .ReadStreamAsync( Direction.Forwards, streamName, StreamPosition.Start)
+                .ForEachAsync( ev=> events.Add(DeSerializeEvent(ev.Event.Data)) );
             return Ok(events);
         }
         catch (Exception ex)
@@ -89,44 +66,27 @@ using System.Text.Json.Serialization;
             }
         }
 
-        // Utility method to serialize event data to JSON or any other format
-        private byte[] SerializeEvent(object eventData)
-        {
-            // Implement serialization logic here
-            return System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(eventData);
-        }
-
     // Utility method to serialize event data to JSON or any other format
-    private WeatherForecast? DeSerializeEvent(byte[]eventData)
+    private byte[] SerializeEvent(object eventData)
     {
         // Implement serialization logic here
-        return System.Text.Json.JsonSerializer.Deserialize<WeatherForecast>(eventData);
+        return System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(eventData);
     }
 
-
-
-    public static byte[]
-    ConcatenateReadOnlyMemories(params ReadOnlyMemory<byte>[] readOnlyMemories)
+    // Utility method to serialize event data to JSON or any other format
+    private WeatherForecast? DeSerializeEvent(ReadOnlyMemory<byte> byteArray)
     {
-        // Calculate the total length of all ReadOnlyMemory<byte> instances
-        int totalLength = 0;
-        foreach (var memory in readOnlyMemories)
+        // Deserialize JSON with custom options
+        var options = new JsonSerializerOptions
         {
-            totalLength += memory.Length;
-        }
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters = { new JsonStringEnumConverter() }
+            // Optional: Include this line if you have enums in your object
+        };
 
-        // Create a byte array to hold the concatenated bytes
-        byte[] byteArray = new byte[totalLength];
-
-        // Copy bytes from each ReadOnlyMemory<byte> to the byte array
-        int currentIndex = 0;
-        foreach (var memory in readOnlyMemories)
-        {
-            memory.Span.CopyTo(byteArray.AsSpan().Slice(currentIndex));
-            currentIndex += memory.Length;
-        }
-
-        return byteArray;
+        string decodedString = Encoding.UTF8.GetString(byteArray.ToArray());
+        WeatherForecast? weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(decodedString, options);
+        return weatherForecast;
     }
 }
  
